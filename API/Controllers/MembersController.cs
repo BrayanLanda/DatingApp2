cb5295ffc0eab61cfadd5acd478e0023767bd788
Brayan Acosta <landakkpp@gmail.com>
2026-09-16 16:26:13 -0500

    feat: implement member update functionality and add member update DTO; enhance member profile component

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,11 +1,14 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
+
 [Authorize]
 public class MembersController(IMemberRepositoty memberRepository) : BaseApiController
 {
@@ -22,10 +25,30 @@ public class MembersController(IMemberRepositoty memberRepository) : BaseApiCont
         if (member == null) return NotFound();
         return member;
     }
-    
+
     [HttpGet("{id}/photos")]
     public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
     {
         return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+    {
+        var memberId = User.GetMemberId();
+        var member = await memberRepository.GetMemberForUpdate(memberId);
+        if (member == null) return BadRequest("Could not get member");
+
+        member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+        member.Description = memberUpdateDto.Description ?? member.Description;
+        member.City = memberUpdateDto.City ?? member.City;
+        member.Country = memberUpdateDto.Country ?? member.Country;
+
+        member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+        memberRepository.Update(member);
+        if (await memberRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update member");
     }
 }
--- client/src/app/features/members/member-profile/member-profile.ts
+++ client/src/app/features/members/member-profile/member-profile.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../../types/member';
+import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
 import { MemberService } from '../../../core/services/member-service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-profile',
@@ -11,13 +12,21 @@ import { MemberService } from '../../../core/services/member-service';
   styleUrl: './member-profile.css',
 })
 export class MemberProfile implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
   private route = inject(ActivatedRoute);
   protected memberService = inject(MemberService);
   protected member = signal<Member | undefined>(undefined);
+  protected editableMember?: EditableMember;
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
       this.member.set(data['member']);
-    })
+      this.editableMember = {
+        displayName: this.member()?.displayName || '',
+        description: this.member()?.description || '',
+        city: this.member()?.city || '',
+        country: this.member()?.country || ''
+      };
+    });
   }
 }
